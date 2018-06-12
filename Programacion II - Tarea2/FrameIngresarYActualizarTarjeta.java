b348e7109d218d85f117ae24c55c8c5d47686259
Elier Cortés Baltodano <39711580+Elier-CoBa@users.noreply.github.com>
2018-06-11 20:09:28 -0600

    Update FrameIngresarYActualizarTarjeta.java

--- Programacion II - Tarea2/FrameIngresarYActualizarTarjeta.java
+++ Programacion II - Tarea2/FrameIngresarYActualizarTarjeta.java
@@ -1,6 +1,3 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,8 +6,13 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class FrameIngresarYActualizarTarjeta extends JFrame {
+public class FrameTarjeta extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtTarjeta;
@@ -23,9 +25,11 @@ public class FrameIngresarYActualizarTarjeta extends JFrame {
 	private JLabel lblSaldoDlares;
 	private JTextField txtSaldoDolares;
 	private JLabel lblMarca;
+	private JLabel lblImagen;
+	private JButton btnRegresar;
 
 	
-	public FrameIngresarYActualizarTarjeta(String tituloFrame, String tituloBoton) {
+	public FrameTarjeta(String tituloFrame, String tituloBoton) {
 		setTitle(tituloFrame);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 449);
@@ -33,6 +37,9 @@ public class FrameIngresarYActualizarTarjeta extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
 		
 		JLabel lblNumTarjeta = new JLabel("N\u00FAmero de Tarjeta:");
 		lblNumTarjeta.setBounds(26, 29, 113, 14);
@@ -61,9 +68,9 @@ public class FrameIngresarYActualizarTarjeta extends JFrame {
 		contentPane.add(txtFechaEmision);
 		txtFechaEmision.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Fecha de vencimiento:");
-		lblNewLabel.setBounds(26, 159, 122, 14);
-		contentPane.add(lblNewLabel);
+		JLabel lblFecha = new JLabel("Fecha de vencimiento:");
+		lblFecha.setBounds(26, 159, 145, 14);
+		contentPane.add(lblFecha);
 		
 		lblCodigoSeguridad = new JLabel("Codigo Seguridad:");
 		lblCodigoSeguridad.setBounds(26, 197, 113, 14);
@@ -98,12 +105,33 @@ public class FrameIngresarYActualizarTarjeta extends JFrame {
 		
 		JComboBox cmbxMarca = new JComboBox();
 		cmbxMarca.setModel(new DefaultComboBoxModel(new String[] {"VISA", "MASTERCARD", "AMERICAN EXPRESS"}));
-		cmbxMarca.setBounds(148, 329, 86, 20);
+		cmbxMarca.setBounds(148, 329, 113, 20);
 		contentPane.add(cmbxMarca);
 		
 		JButton btnIngresar = new JButton(tituloBoton);
-		btnIngresar.setBounds(21, 377, 89, 23);
+		btnIngresar.setBounds(21, 377, 118, 23);
 		contentPane.add(btnIngresar);
+		
+		btnRegresar = new JButton("Regresar");
+		btnRegresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnRegresar.setIcon(new ImageIcon(FrameTarjeta.class.getResource("/Imagenes/back.png")));
+		btnRegresar.setBounds(321, 386, 113, 23);
+		contentPane.add(btnRegresar);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBounds(10, 11, 272, 398);
+		panel.setOpaque(false);
+		contentPane.add(panel);
+		
+		lblImagen = new JLabel("New label");
+		lblImagen.setIcon(new ImageIcon(FrameTarjeta.class.getResource("/Imagenes/FondoFrame.jpg")));
+		lblImagen.setBounds(-13, -14, 472, 437);
+		contentPane.add(lblImagen);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
 }
